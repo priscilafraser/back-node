7eb447306bcdaf53ffdc5df9df5a573d86280ef0
Priscila Miranda (fraser) <55546267+priscilafraser@users.noreply.github.com>
2022-09-11 22:32:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors')
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
